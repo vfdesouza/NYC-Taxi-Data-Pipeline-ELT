b69b0b3c46b50dafe55ddb94cec63f081d6a839e
vfdesouza <vfdesouza@outlook.com>
2023-08-13 18:10:46 -0300

    criacao e configuracao dos arquivos .yml para inclusao do dbt no projeto - primeiros testes iniciados e tudo certo ate entao

---
+++ nyc_dbt/models/marts/marts_model_test.sql
@@ -0,0 +1,29 @@
+{{ config(
+    materialized='incremental',
+    unique_key=['VendorID', 'tpep_pickup_datetime'],
+    partition_by={
+        "field": "extract_at",
+        "data_type": "date",
+        "granularity": "day"
+    }
+) }}
+
+WITH
+  source AS (
+    SELECT
+      *
+    FROM
+      {{ ref('stg_model_test') }}
+  )
+
+SELECT * FROM source
+
+{% if is_incremental() %}
+  WHERE
+  extract_at > (
+    SELECT
+      MAX(extract_at)
+    FROM
+      {{ this }}
+  )
+{% endif %}
